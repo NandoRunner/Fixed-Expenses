8cd86b7fbea58314afa2b9dc21b88d19506f8324
Fernando Andrade <fernando@hrestart.com.br>
2021-12-04 10:52:27 -0300

    Version update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  CURRENT_VERSION: '0.21.7',
+  CURRENT_VERSION: '0.21.8',
   hostingName: 'Fixed Expenses',
   icon: 'calculator',
   init_page: 'water',
